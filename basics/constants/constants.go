dea2d3b46b7fbb87b40738b9fb7ace8289a17c3c
PedroB <bereilhp@gmail.com>
2025-03-19 19:34:03 +0100

    multiple const defined

--- basics/constants/constants.go
+++ basics/constants/constants.go
@@ -7,7 +7,16 @@ import (
 const PI = 3.14
 const A int = 1
 
+const (
+	B int     = 1
+	C float32 = 3.14
+	D         = "Hi!"
+)
+
 func main() {
 	fmt.Println(PI)
 	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
+	fmt.Println(D)
 }
