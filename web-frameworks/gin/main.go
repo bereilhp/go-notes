5d34b909ebc08734cf4d2f42459acbce4cd12e7f
PedroB <bereilhp@gmail.com>
2025-04-07 16:39:49 +0200

    Simple POST

--- web-frameworks/gin/main.go
+++ web-frameworks/gin/main.go
@@ -6,6 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Message struct {
+	Content string `json:"content"`
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -13,5 +17,20 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.POST("/message", func(c *gin.Context) {
+		var msg Message
+
+		// Bind JSON body to the Message struct
+		if err := c.ShouldBindJSON(&msg); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Send a response
+		c.JSON(http.StatusOK, gin.H{
+			"received": msg.Content,
+		})
+	})
+
 	r.Run()
 }
